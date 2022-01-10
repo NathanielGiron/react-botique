77e4ceefbabc7d15b8823d6827cb98fce0754099
Nathaniel Giron <nathaniel.giron@gmail.com>
2022-01-10 12:38:20 -0800

    finish up authentication and fixed styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
+
+import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>REACT-BOTIQUE</Link>
     <div className='options'>
@@ -14,6 +15,17 @@ const Header = () => (
       <Link className='option' to='/contact'>
         CONTACT
       </Link>
+      {
+        currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )
+      }
     </div>
   </div>
 );
