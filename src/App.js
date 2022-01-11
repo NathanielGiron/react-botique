85c7c23ba80b218ec585358d325762cff94077f4
Nathaniel Giron <nathaniel.giron@gmail.com>
2022-01-11 10:58:01 -0800

    store user in firebase when logging in and store data in app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SigninAndSignupPage from './pages/signin-and-signup/signin-and-signup.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
   constructor() {
@@ -21,10 +21,20 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {console.log(this.state)});
+        })
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -8,7 +8,6 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     className={`${size} menu-item`} 
     onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
-    {console.log(linkUrl)}
     <div 
       className='background-image'
       style={{
