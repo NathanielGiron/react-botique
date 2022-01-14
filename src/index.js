3d554106843bc8d1b09e46270da664e42b2f2653
Nathaniel Giron <nathaniel.giron@gmail.com>
2022-01-13 22:39:10 -0800

    Finish setting up redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
