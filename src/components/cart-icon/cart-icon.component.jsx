ed4ff90520260d72c88aedfe78244695e4f8cb77
Nathaniel Giron <nathaniel.giron@gmail.com>
2022-01-17 21:01:45 -0800

    display number of items in cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,24 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <span className='cart-text'>CART</span>
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+  )
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(CartIcon);
