4dc1faf0d6d9e006b11b815a0e9f9cf9de8380e7
Nathaniel Giron <nathaniel.giron@gmail.com>
2022-01-22 23:33:32 -0800

    style checkout items

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
@@ -26,7 +28,9 @@ const CheckoutPage = ({ cartItems, total }) => (
       </div>
     </div>
     {
-      cartItems.map(cartItem => cartItem.name )
+      cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))
     }
     <div className='total'>
       <span>Total: ${total}</span>
