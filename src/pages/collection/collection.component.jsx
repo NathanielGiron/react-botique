042e1c74a2725ea338885a235d1853d1d9de65b4
Nathaniel Giron <nathaniel.giron@gmail.com>
2022-01-24 01:38:46 -0800

    nested routing in shop page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className='collection'>
+    <h2>Collection Page</h2>
+  </div>
+);
+
+export default CollectionPage;
