35e5eacaa0465d70602fcf32f1cac328e1ef0f44
Nathaniel Giron <nathaniel.giron@gmail.com>
2022-01-24 01:57:43 -0800

    collection routing and selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
   <div className='collection'>
     <h2>Collection Page</h2>
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
